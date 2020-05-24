485afcb7727227dbe0aa59f42d20cd24fcb03ec1
Joonehee-JO <63311325+Joonehee-JO@users.noreply.github.com>
2020-05-24 22:07:22 +0900

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def male(request):
-    return render(request, 'male.html')
-
-
-def female(request):
-    return render(request, 'female.html')
